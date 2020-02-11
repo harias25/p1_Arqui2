046c67191687fed565dc0c80d86fc6560bf7ee81
Haroldo Arias <ihpam25@gmail.com>
2020-02-11 00:13:54 -0600

    Cambio de CORS

--- apiArqui2/Startup.cs
+++ apiArqui2/Startup.cs
@@ -19,12 +19,22 @@ namespace apiArqui2
         {
             Configuration = configuration;
         }
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder.WithOrigins("*").AllowAnyHeader()
+                                .AllowAnyMethod(); ;
+                });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -40,7 +50,7 @@ namespace apiArqui2
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseCors(MyAllowSpecificOrigins);
             app.UseHttpsRedirection();
             app.UseMvc();
         }
